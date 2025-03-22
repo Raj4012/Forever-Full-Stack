f315e74d0611008896ca2cacce56cbe676958e9a
Rajkumar Bharti <155239177+Raj4012@users.noreply.github.com>
2025-03-22 16:25:01 +0530

    Update LatestCollection.jsx

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -16,7 +16,7 @@ const LatestCollection = () => {
       <div className="text-center py-8 text-3xl">
         <Title text1={"LATEST"} text2={"COLLECTIONS"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          Stay ahead of the trends with our Latest Collections! ✨ Explore newly launched products, fresh styles, and innovative picks curated just for you
+          Stay ahead of the trends with our Latest Collections!Explore newly launched products, fresh styles, and innovative picks curated just for you
         </p>
       </div>
 
